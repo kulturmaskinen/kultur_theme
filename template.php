31267e7af28164143c42fd92a18d17d6b50ed089
Amer <impara1@gmail.com>
2015-03-26 15:05:20 +0100

    Added media selector and fixed background function.

--- template.php
+++ template.php
@@ -21,9 +21,9 @@ function kultur_theme_preprocess_html(&$vars) {
   $front = "front";
   if (drupal_match_path($path, $front)) {
   $node = node_load_by_title('forside baggrund', 'background');
-  if (!empty($node) && !empty($node->field_image)) {
-    $bg1200 = file_create_url($node->field_image[LANGUAGE_NONE][0]['uri']);
-    $bg1600 = file_create_url($node->field_image[LANGUAGE_NONE][1]['uri']);
+  if (!empty($node) && !empty($node->field_min_1600px) && !empty($node->field_min_1200px)) {
+    $bg1200 = file_create_url($node->field_min_1200px[LANGUAGE_NONE][0]['uri']);
+    $bg1600 = file_create_url($node->field_min_1600px[LANGUAGE_NONE][0]['uri']);
     
       drupal_add_css(
           '@media screen and (max-width: 1200px) { body { background-size: 100% 100%; background-repeat: no-repeat; background-image:url(' . $bg1200 . ');} }', 'inline'
