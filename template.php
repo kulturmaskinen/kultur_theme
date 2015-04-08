d9dd4e3dc2f87fe54da6e28bbcf0dad9b096a932
Amer <impara1@gmail.com>
2015-04-08 09:59:13 +0200

    Uptimized dynamic background function.

--- template.php
+++ template.php
@@ -20,17 +20,18 @@ function kultur_theme_preprocess_html(&$vars) {
   $path = drupal_get_path_alias();
   $front = "front";
   if (drupal_match_path($path, $front)) {
-  $node = node_load_by_title('forside baggrund', 'background');
+    $node = node_load_by_title('forside baggrund', 'background');
+  }
+
   if (!empty($node) && !empty($node->field_min_1600px) && !empty($node->field_min_1200px)) {
     $bg1200 = file_create_url($node->field_min_1200px[LANGUAGE_NONE][0]['uri']);
     $bg1600 = file_create_url($node->field_min_1600px[LANGUAGE_NONE][0]['uri']);
-    
-      drupal_add_css(
-          '@media screen and (max-width: 1200px) { body { background-size: 100% 100%; background-repeat: no-repeat; background-image:url(' . $bg1200 . ');} }', 'inline'
-      );
-      drupal_add_css(
-          '@media screen and (min-width: 1200px) { body { background-size: 100% 100%; background-repeat: no-repeat; background-image:url(' . $bg1600 . ');} }', 'inline'
-      );
-    }
+
+    drupal_add_css(
+        '@media screen and (max-width: 1200px) { body { background-size: 100% 100%; background-repeat: no-repeat; background-image:url(' . $bg1200 . ');} }', 'inline'
+    );
+    drupal_add_css(
+        '@media screen and (min-width: 1200px) { body { background-size: 100% 100%; background-repeat: no-repeat; background-image:url(' . $bg1600 . ');} }', 'inline'
+    );
   }
 }
