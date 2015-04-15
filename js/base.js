8b03058b9cf9a722e4d6ebec6e9d85be53551407
Amer <impara1@gmail.com>
2015-04-15 09:12:17 +0200

    Node styling, and footer position fix.

---
+++ js/base.js
@@ -0,0 +1,12 @@
++function ($) {
+
+
+$(document).ready(function($) {
+    $('.panels-flexible-region-site_template-center').css("height", $(document).height() - 284);
+    
+});
+
+
+
+
+}(jQuery);
--- template.php
+++ template.php
@@ -75,3 +75,61 @@ return '
 <ul class="nav navbar-nav" style="margin: 0 0 0 1em;">' . $vars['tree'] . ''
 . '</ul><div class="navbar-form navbar-right">' . drupal_render($form) . '</div>';
 }
+
+/**
+ * Implements hook_preprocess_node().
+ *
+ * Override or insert variables into the node templates.
+ *
+ * @param $variables
+ *   An array of variables to pass to the theme template.
+ * @param $hook
+ *   The name of the template being rendered ("node" in this case.)
+ */
+function kultur_theme_preprocess_node(&$variables, $hook) {
+  //added open graph meta tags for facebook.
+  $site_name = variable_get('site_name');
+  $og_title = $variables['node']->title . ($site_name ? ' | ' . $site_name : '');
+  if ($variables['type'] == 'news') {
+    $og_description = isset($variables['node']->field_lead[LANGUAGE_NONE][0]) ? drupal_substr(check_plain(strip_tags($variables['node']->field_lead[LANGUAGE_NONE][0]['safe_value'])), 0, 100) . '..' : '';
+    $og_image = isset($variables['node']->field_title_image[LANGUAGE_NONE][0]) ? file_create_url($variables['node']->field_title_image[LANGUAGE_NONE][0]['uri'], array('absolute' => TRUE)) : '';
+  
+    drupal_add_html_head(array(
+    '#tag' => 'meta',
+    '#attributes' => array(
+      'property' => 'og:title',
+      'content' => $og_title,
+    ),
+      ), 'node_' . $variables['node']->nid . '_og_title');
+  drupal_add_html_head(array(
+    '#tag' => 'meta',
+    '#attributes' => array(
+      'property' => 'og:description',
+      'content' => $og_description,
+    ),
+      ), 'node_' . $variables['node']->nid . '_og_description');
+
+  drupal_add_html_head(array(
+    '#tag' => 'meta',
+    '#attributes' => array(
+      'property' => 'og:image',
+      'content' => $og_image,
+    ),
+      ), 'node_' . $variables['node']->nid . '_og_image');
+  }
+  // Add ddbasic_byline to variables
+  $variables['kultur_theme_byline'] = t('By: ');
+  
+  /**
+   * @TODO Use date-formats defined in the backend, do not hardcode formats...
+   *       ever
+   */
+  // Add updated to variables.
+  $variables['kultur_theme_updated'] = t('Updated: !datetime', array('!datetime' => format_date($variables['node']->changed, 'custom', 'l j. F Y')));
+
+  // Modified submitted variable
+  if ($variables['display_submitted']) {
+    $variables['submitted'] = t('Submitted: !datetime', array('!datetime' => format_date($variables['created'], 'custom', 'l j. F Y')));
+  }
+  
+}
