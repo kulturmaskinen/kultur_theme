2af10dd22ea8fc05664a88afe8d234dc7132931f
Amer <impara1@gmail.com>
2015-05-21 13:24:26 +0200

    Main menu dropdown styling and fix.

--- template.php
+++ template.php
@@ -210,6 +210,51 @@ function kultur_theme_place2book_ticketsinfo($variables) {
   return $output;
 }
 
+function kultur_theme_menu_link(array $variables) {
+  $element = $variables['element'];
+  $sub_menu = '';
+
+  if ($element['#below']) {
+    // Prevent dropdown functions from being added to management menu so it
+    // does not affect the navbar module.
+    if (($element['#original_link']['menu_name'] == 'management') && (module_exists('navbar'))) {
+      $sub_menu = drupal_render($element['#below']);
+    }
+    else{
+    unset($element['#below']['#theme_wrappers']);
+    $sub_menu = '<ul class="dropdown-menu">' . drupal_render($element['#below']) . '</ul>';
+    $element['#localized_options']['attributes']['class'][] = 'dropdown-toggle disabled';
+    $element['#localized_options']['attributes']['data-toggle'] = 'dropdown';
+$element['#localized_options']['attributes']['data-hover'] = 'dropdown';
+$element['#localized_options']['attributes']['data-delay'] = '100';
+$element['#localized_options']['attributes']['data-close-others'] = 'false';
+
+    // Check if this element is nested within another
+    if ((!empty($element['#original_link']['depth'])) && ($element['#original_link']['depth'] > 1)) {
+      // Generate as dropdown submenu
+      $element['#attributes']['class'][] = 'dropdown-submenu';
+  $element['#localized_options']['attributes']['tabindex'][] = '-1';
+
+    }
+    else {
+      // Generate as standard dropdown
+      $element['#attributes']['class'][] = 'dropdown';
+      $element['#localized_options']['html'] = TRUE;
+      $element['#title'] .= '<span class="caret"></span>';
+    }
+  }
+  // Set dropdown trigger element to # to prevent inadvertant page loading with submenu click
+   $element['#localized_options']['attributes']['data-target'] = '#';
+  }
+  // On primary navigation menu, class 'active' is not set on active menu item.
+  // @see https://drupal.org/node/1896674
+  if (($element['#href'] == $_GET['q'] || ($element['#href'] == '<front>' && drupal_is_front_page())) && (empty($element['#localized_options']['language']))) {
+    $element['#attributes']['class'][] = 'active';
+  }
+  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+}
+
 /**
  * Declare various hook_*_alter() hooks.
  *
