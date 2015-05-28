4a680fd7bb15839f7907aa55b63a3ca7a441047f
Amer <impara1@gmail.com>
2015-05-28 11:38:56 +0200

    Row stacked reordering changed selector

--- js/base.js
+++ js/base.js
@@ -1,23 +1,25 @@
 +function ($) {
 
-
-$(document).ready(function($) {
+    $(document).ready(function ($) {
         $('.panels-flexible-region-container-fluid-center-inside').css("height", $(document).height() - 270);
-    
-        });
 
+        checkSize();
+// run test on resize of the window
+        $(window).resize(checkSize);
 
-    $(window).on("resize", function () {
+    });
+
+    function checkSize() {
 
         if ($(window).width() < 768) {
+
             $('.panels-flexible-region-node_view-center').prependTo($('.panels-flexible-region-node_view-left').parent());
-        }
-        else {
+
+        } else {
 
             $('.panels-flexible-region-node_view-left').prependTo($('.panels-flexible-region-node_view-center').parent());
 
         }
-
-    }).resize();
+    }
 
 }(jQuery);
