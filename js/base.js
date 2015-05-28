81f24b1eac8d23b9415123608a838655eb91a785
Amer <impara1@gmail.com>
2015-05-28 11:03:01 +0200

    Row stacked reordering, added media queries breakepoints.

--- js/base.js
+++ js/base.js
@@ -2,11 +2,22 @@
 
 
 $(document).ready(function($) {
-    $('.panels-flexible-region-container-fluid-center-inside').css("height", $(document).height() - 270);
+        $('.panels-flexible-region-container-fluid-center-inside').css("height", $(document).height() - 270);
     
-});
+        });
 
 
+    $(window).on("resize", function () {
 
+        if ($(window).width() < 768) {
+            $('.panels-flexible-region-node_view-center').prependTo($('.panels-flexible-region-node_view-left').parent());
+        }
+        else {
+
+            $('.panels-flexible-region-node_view-left').prependTo($('.panels-flexible-region-node_view-center').parent());
+
+        }
+
+    }).resize();
 
 }(jQuery);
