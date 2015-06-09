8c5a54f7534d5f85ee51a0886fc6493b4d5445c3
Amer <impara1@gmail.com>
2015-06-09 12:36:12 +0200

    Dropdown only on top level main menu.

--- js/base.js
+++ js/base.js
@@ -1,7 +1,7 @@
 +function ($) {
 
     $(document).ready(function ($) {
-      
+      $( "div ul" ).not( ".menu-level-1 ul" ).removeClass('dropdown-menu');
         checkSize();
 // run test on resize of the window
         $(window).resize(checkSize);
