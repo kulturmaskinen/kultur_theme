76c09156f96bdb41b10ae281e831c7218f878e84
Amer <impara1@gmail.com>
2015-06-10 15:32:57 +0200

    Styled the content blocks for iphone

--- js/base.js
+++ js/base.js
@@ -15,12 +15,14 @@
         if ($(window).width() < 768) {
 
             if ($(window).width() < 768) {
-
+            
         $('.col-md-3.col-xs-12.col-sm-height.col-md-height.col-xs-height.col-full-height.col-top').prependTo($('.col-xs-12.col-sm-6.col-md-8.col-xs-height.col-full-height').parent());
-                } else {
+           $('h2').prependTo($('.col-xs-12.col-sm-6.col-md-8.col-xs-height.col-full-height').parent());     
+            } else {
 
         $('.col-xs-12.col-sm-6.col-md-8.col-xs-height.col-full-height').prependTo($('.col-md-3.col-xs-12.col-sm-height.col-md-height.col-xs-height.col-full-height.col-top').parent());
-                }
+              $('.col-md-3.col-xs-12.col-sm-height.col-md-height.col-xs-height.col-full-height.col-top').prependTo($('h2').parent());  
+            }
 
         $('.panels-flexible-region-node_view-center').prependTo($('.panels-flexible-region-node_view-left').parent());
                 } else {
