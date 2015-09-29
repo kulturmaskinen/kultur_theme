4feab94489c2012f91780da40492820c9bdbbf70
Amer <impara1@gmail.com>
2015-09-29 12:11:23 +0200

    Resized the iframe to 90%

--- js/base.js
+++ js/base.js
@@ -16,7 +16,7 @@
 	}	
         
         if ($('iframe').exists()) {
-            $('iframe').attr('style', 'position: absolute; left: 0px; top: 0px; width: 100%; height: 100%');
+            $('iframe').attr('style', 'position: absolute; left: 0px; top: 0px; width: 90%; height: 100%');
             $('iframe').parent().attr('style', 'position: relative; width: 100%; height: 0px; padding-bottom: 60%;');
         }
         
