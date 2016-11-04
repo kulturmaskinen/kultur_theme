51bea33654baf75be2df526bafa78d02c03adc20
impara <impara1@gmail.com>
2016-11-04 09:38:06 +0100

    Responsive sub menu fix.

--- template.php
+++ template.php
@@ -211,26 +211,23 @@ function kultur_theme_menu_tree__main_menu($vars) {
 <nav class="navbar navbar-default" role="navigation">
   <div class="container-fluid">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-4">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
+      <button type="button" class="navbar-toggle" data-toggle="collapse" style="width:90%;background-color:#DDDDDD;" data-target="#bs-example-navbar-collapse-4">
+        Undermenu <span class="caret"></span>
       </button>
       <a class="navbar-brand" href="#"></a>
     </div>
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-4">
       <ul class="nav navbar-nav">
         <li class="dropdown">
-          <ul class="dropdown-menu">
-            ' . $vars['tree'] . ''
+  <ul class="dropdown-menu">
+    ' . $vars['tree'] . ''
         . '
-          </ul>
+  </ul>
         </li>
       </ul>
     </div>
   </div>
-</nav>;';
+</nav>';
   }
 }
 
